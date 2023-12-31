7cd29a8d7f47c433ca5116be94e0aa82a36c405c
Kevin Soruco <kevin-s93@hotmail.com>
2023-12-31 14:46:45 -0300

    feat: add events to manipulate sshKeys
    * Show sshKeys in a li element
    * Add onclick event to each one of them and the copy command

--- renderer.js
+++ renderer.js
@@ -1,19 +1,37 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+var exec = require("child_process").exec;
 
 const homeDirectory = os.homedir();
 const sshDirectory = path.join(homeDirectory, process.env.SSH_DIRECTORY);
+const copySshKeyCommand = (directory, sshKeyName) => `cat ${directory}/${sshKeyName} | pbcopy`
 
-document.getElementById('btn').addEventListener('click', () => {
-    try {
-        const files = fs.readdirSync(sshDirectory);
+document.getElementById("btn").addEventListener("click", () => {
+  try {
+    const files = fs.readdirSync(sshDirectory);
+    const publicSshFiles = files.filter((file) => file.endsWith(".pub"));
 
-       const publicSshFiles = files.filter((file) => file.endsWith('.pub'));
+    const generalList = document.getElementById("files-location");
 
-       document.getElementById('files-location').innerText = publicSshFiles.join('\n');
+    publicSshFiles.forEach((pubSshKey) => {
+      const li = document.createElement("li");
+      generalList.appendChild(li).innerText = pubSshKey;
+      li.onclick = () => {
+        let child;
 
-    } catch(e) {
-        console.error('Error reading directory: ', e)
-    }
+        child = exec(
+        copySshKeyCommand(sshDirectory, pubSshKey),
+          function (error, stdout, stderr) {
+            console.log("stdout: " + stdout);
+            if (error !== null) {
+              console.log("exec error: " + error);
+            }
+          }
+        );
+      };
+    });
+  } catch (e) {
+    console.error("Error reading directory: ", e);
+  }
 });
